4023378bcb0d8fda233f87439b173fef20e9055b
Richard Webber <rikki.webber@gmail.com>
2023-11-27 15:16:01 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# lab-baseball-cards
---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import playerData from './playerData.js'
+import  { useState } from 'react'
+
+
+function BaseballCard(props) {
+  const [showPicture, setShowPicture] = useState(true);
+
+  const toggleCard = () => {
+    setShowPicture(!showPicture)
+  }
+
+  if (showPicture) {
+    return (
+      <div className="card" onClick={toggleCard}>
+        <h2>{props.name}</h2>
+        <img src={props.imgUrl} alt={props.name} />
+      </div>
+    );
+  } else {
+    const statsDisplay = Object.entries(props.stats).map(([statName, statValue]) => (
+      <p key={statName}>
+        {statName}: {statValue}
+      </p>
+    ));
+  
+    return (
+      <div className='card' onClick={toggleCard}>
+        <h2>{props.name}</h2>
+        <p>Team: {props.team}</p>
+        <p>Position: {props.position}</p>
+        {statsDisplay}
+      </div>
+    );
+  }
+}
+
+
+
+function App() {
+
+  const cards = playerData.map((player) => (
+  <BaseballCard 
+  name={player.name}
+  team={player.team}
+  position={player.position}
+  stats={player.stats}
+  imgUrl={player.imgUrl}
+  id={player.cardId}
+  />
+  ));
+
+  return <>{cards}</>
+}
+
+export default App;
+
